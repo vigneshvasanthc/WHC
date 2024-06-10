3d4f8df477394d58fbe4e9954c36dc39003589d8
vignesh vasanth <vigneshvasanth.chandrasekar@trustrace.com>
2024-06-10 07:19:37 +0530

    new repositiory is created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideNavBarComponent } from './side-nav-bar/side-nav-bar.component';
+import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavBarComponent,
+    TopNavBarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
